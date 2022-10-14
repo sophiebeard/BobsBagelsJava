a00629e170f11632cb929259ae7d70d13b5fec12
ramz196 <ramzansamura@gmail.com>
2022-10-14 12:55:58 +0100

    updated bobs begels

--- src/main/java/Bagel.java
+++ src/main/java/Bagel.java
@@ -1,2 +1,7 @@
+import java.util.Arrays;
+import java.util.List;
 public class Bagel {
+    public void basket() {
+        List basketItems = new ArrayList();
+    }
 }
--- src/test/java/BasketTest.java
+++ src/test/java/BasketTest.java
@@ -8,7 +8,13 @@ public class BasketTest {
     @DisplayName("A bagel has been added to the basket")
     void addBagel() {
         // Arrange
+        basket = new Basket();
+        item = { id: 'item1' };
+        expected = 1;
 
+        //Act
+        basket.add(item1);
+        actual = basket.basketItems.length;
         // Assert
         assertEquals(Basket.length(), 1);
 
